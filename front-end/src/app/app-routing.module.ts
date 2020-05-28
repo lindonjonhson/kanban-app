26534f983a2fd8797ff157cf2c940511db49812f
Lindon Jonhson <lindon.jonhsonf@gmail.com>
2020-05-28 10:58:32 +0100

    Added new board button and route

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NewBoardComponent } from './pages/new-board/new-board.component';
 import { BoardViewComponent } from './pages/board-view/board-view.component';
 import { MainViewComponent } from './pages/main-view/main-view.component';
 import { NgModule } from '@angular/core';
@@ -7,7 +8,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'boards', pathMatch: 'full' },
   { path: 'boards', component: BoardViewComponent },
-  { path: 'boards/:id', component: MainViewComponent }
+  { path: 'boards/:id', component: MainViewComponent },
+  { path: 'new-board', component: NewBoardComponent }
 ];
 
 @NgModule({
